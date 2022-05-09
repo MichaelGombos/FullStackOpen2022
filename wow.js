92aba183b920841377f3b710d1f88d3af9e1aa39
EC2 Default User <ec2-user@ip-172-31-48-176.us-west-2.compute.internal>
2022-05-09 01:02:35 +0000

    Completed Exersize 1.3-1.5

---
+++ wow.js
@@ -0,0 +1,31 @@
+console.log("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+
+
+function product(a,b){
+    return a*b
+}
+
+const result = product(2,6);
+
+console.log(result);
+
+let coolProduct = function(a,b) {
+    return (a*b);
+}
+
+let evenCoolerProduct = (a,b) => a*b;
+
+let coolResult = coolProduct(2,6);
+
+let evenCooler = evenCoolerProduct(2,6);
+
+
+
+console.log(coolProduct);
+
+console.log(evenCooler)
+
+
+
+
+console.log("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
